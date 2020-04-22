5cec584f0bd8eed31e8be922055aa5c2c06db37a
Tong Yu <yutongsf@gmail.com>
2020-04-22 15:33:25 -0700

    reset static folder

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const log = console.log;
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 const items = require('./routes/api/items');
 
 // bodyParser middleware:
@@ -32,14 +32,16 @@ if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
+  app.use('*', express.static(path.join(__dirname, 'client', 'build')));
 }
 
 // app.get('/', (req, res) => {
 //   res.send('hello..!');
 // });
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   log(`server is running on port ${PORT}`);
